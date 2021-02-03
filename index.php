92cfa3d2f14e9187617928e6cb4d30db51101fe5
Lrd134 <lches3200@gmail.com>
2021-02-03 18:07:42 -0500

    cleaning

--- index.php
+++ index.php
@@ -1,35 +1,48 @@
-# Begin index.php
-# Chesbro's Catalog
-# Maintained by Lawrence Chesbro, Jr.
-# Github:
+<!-- Begin index.php
+ Chesbro's Catalog
+ Maintained by Lawrence Chesbro, Jr.
+ Github: https://github.com/Lrd134/chesbroscatalog/
+ -->
 <html> 
 <head>
 <title>Welcome to Chesbro's Catalog</title> 
 <link href="layout.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-# WelcomeMsg
+<!-- begin welcome-msg -->
 <div>
-<span class="welcomeMsg"> Hello, Welcome to my First Website!</span>
+<span class="welcome-msg"> Hello, Welcome to my First Website!</span>
 </div>
-
+<!-- end welcome-msg -->
+<!-- begin doge-meme -->
 <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" title="Do it for the memes">
 <img src="https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2014/1/20/1390229376041/1773f12e-7ddc-4f65-ae31-ef0fbba4f4d4-620x372.png?width=300&quality=45&auto=format&fit=max&dpr=2&s=23c02f3059ad62faf973df2ede8608f1"
  alt="Click here for the meme's">
 </a>
+<!-- end doge-meme -->
 <br>
-<a href="https://www.paypal.me/GamingDecoded"><img src="https://i.gyazo.com/7efc7f0135beb22c7635cf083ce3ecd5.png" style="width:128px;height:64px;" alt="Donate" title="Thanks for considering!"></a>
+<!-- begin donate-button -->
+<a href="https://www.paypal.me/GamingDecoded">
+<img src="https://i.gyazo.com/7efc7f0135beb22c7635cf083ce3ecd5.png" style="width:128px;height:64px;" alt="Donate" title="Thanks for considering!">
+</a>
+<!-- end donate-button -->
 <br>
-<br>
-<div id="donate_msg">
+<!-- begin donate-msg -->
+<div>
+<span id="donate-msg">
     Hello, I am writing this to you because you
 <br>
     should consider donating to me.
+</span>
 </div>
-<div id="fine_txt">
+<!-- end doante-msg -->
+<!-- begin fine-txt -->
+<div>
+<span id="fine-txt">
 If you choose not to, that is totally alright.
+</span>
 </div>
-
+<!-- end fine-txt -->
 </body>
 
 </html>
