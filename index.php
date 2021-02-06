b1189235ec35a27ad684efe450cc356f56869da0
Lrd134 <lches3200@gmail.com>
2021-02-05 23:53:54 -0500

    v1.0.0.1
    Added a few things that I'm learning, not done with the day but I wanted to save and come back later.

--- index.php
+++ index.php
@@ -2,47 +2,105 @@
  Chesbro's Catalog
  Maintained by Lawrence Chesbro, Jr.
  Github: https://github.com/Lrd134/chesbroscatalog/
- -->
+<!-- -->
 <html> 
-<head>
-<title>Welcome to Chesbro's Catalog</title> 
-<link href="layout.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-<!-- begin welcome-msg -->
-<div>
-<span class="welcome-msg"> Hello, Welcome to my First Website!</span>
-</div>
-<!-- end welcome-msg -->
-<!-- begin doge-meme -->
-<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" title="Do it for the memes">
-<img src="https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2014/1/20/1390229376041/1773f12e-7ddc-4f65-ae31-ef0fbba4f4d4-620x372.png?width=300&quality=45&auto=format&fit=max&dpr=2&s=23c02f3059ad62faf973df2ede8608f1"
- alt="Click here for the meme's">
-</a>
-<!-- end doge-meme -->
-<br>
-<!-- begin donate-button -->
-<a href="https://www.paypal.me/GamingDecoded">
-<img src="https://i.gyazo.com/7efc7f0135beb22c7635cf083ce3ecd5.png" style="width:128px;height:64px;" alt="Donate" title="Thanks for considering!">
-</a>
-<!-- end donate-button -->
-<br>
-<!-- begin donate-msg -->
-<div>
-<span id="donate-msg">
-    Hello, I am writing this to you because you
-<br>
-    should consider donating to me.
-</span>
-</div>
-<!-- end doante-msg -->
-<!-- begin fine-txt -->
-<div>
-<span id="fine-txt">
-If you choose not to, that is totally alright.
-</span>
-</div>
-<!-- end fine-txt -->
-</body>
+    <head>
+        <title>Welcome to Chesbro's Catalog</title> 
+        <link href="layout.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <!-- begin Chesbro's Catalog -->
+        <h1>
+            Chesbro's Catalog<br>
+        </h1>
+        <!-- end Chesbro's Catalog -->
+        
+        <!-- begin welcome-msg -->
+        <div>
+            <span class="welcome-msg"> Hello, Welcome to my First Website!
+            </span>
+        </div>
+        <!-- end welcome-msg -->
+        
+        <!-- begin doge-meme -->
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" title="Do it for the memes">
+            <img src="https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2014/1/20/1390229376041/1773f12e-7ddc-4f65-ae31-ef0fbba4f4d4-620x372.png?width=300&quality=45&auto=format&fit=max&dpr=2&s=23c02f3059ad62faf973df2ede8608f1"
+            alt="Click here for the meme's">
+        </a>
+        <!-- end doge-meme -->
+        <br>
+        <!-- begin donate-button -->
+        <a href="https://www.paypal.me/GamingDecoded">
+            <img src="https://i.gyazo.com/7efc7f0135beb22c7635cf083ce3ecd5.png" style="width:128px;height:64px;" alt="Donate" title="Thanks for considering!">
+        </a>
+        <!-- end donate-button -->
+        <br>
+        <!-- begin donate-msg -->
+        <div>
+            <span id="donate-msg">
+                Hello, I am writing this to you because you
+                <br>
+                should consider donating to me.
+            </span>
+        </div>
+        <!-- end doante-msg -->
+        
+        <!-- begin fine-txt -->
+        <div>
+            <!-- begin fine -->
+            <span class="fine">
+            If you choose not to, that is totally alright.
+            </span>
+            <!-- end fine -->
+            
+            <!-- Begin yellowFineTxt -->
+            <span class="fine yellowtxt">
+            <br> I'm going to try and modify this paragraph without using the yellowtxt identifier. 
+            </span>
+            <!-- end yellowFineTxt -->
+        </div>
+        <!-- end fine-txt -->
+        
+        <!-- begin parent -->
+        <div class="parent">
+            This is the parent.
+            <!-- begin grand-child -->
+            <span id="child" class="grand-child">
+            This is the grandchild.
+                <!-- begin great-grand-child -->
+                <span id="child" class="great-grand-child">
+                This is the great-grandchild.
+                </span>
+                <!-- end great-grand-child -->
+            </span>
+            <!-- end grand-child -->
+        </div>
+        <!-- end parent -->
+        
+        <!-- begin img w/ border -->
+        <img src="https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=900.0&fit=crop"
+            style="width:128px;height:64px;" alt="Cat">
+        </img>
+        <!-- end img w/ border -->
+        
+        <!-- begin General Sibling example -->
+        <div>
+            <!-- begin first -->
+            <span id="first">
+            This one is first.
+            </span>
+            <!-- end first -->
+            
+            <!-- begin second -->
+            <span>
+            This one is second.
+            </span>
+            <!-- end second -->
+        </div>
+        <!-- end General Sibling example -->
+        
+    </body>
+    <!-- end index.php -->
 
 </html>
+<!-- -->
